cf646216b16a77c9ba5aed5169c99b118e646d4c
yathink3 <yathink3@gmail.com>
2023-08-16 00:13:46 +0800

    (feat)ui : navigation bar done

--- src/Pages-components/Navigation.tsx
+++ src/Pages-components/Navigation.tsx
@@ -1,130 +1,62 @@
+import NavItem from '@/components/ui/NavItem';
+import { type IconType } from '@/components/ui/NavItem';
 import InstillLogo from '../assets/instill-logo-1.png';
-import XFrameDashIcon from '../assets/x-base-one-line-frame-dash.svg';
-import XLineChartIcon from '../assets/x-base-one-line-icon.svg';
-import XPieChartIcon from '../assets/x-base-one-line-chart-pie.svg';
-import XOneWaveIcon from '../assets/x-base-one-wave-icon.svg';
-import XPeopleIcon from '../assets/x-base-one-line-people.svg';
-import XBriefCaseIcon from '../assets/x-base-one-line-line-rounded-briefcase.svg';
-import XBulbIcon from '../assets/x-base-one-line-light-bulb.svg';
-import XInstillIcon from '../assets/x-base-one-line-instill-icon.svg';
-import XUserIcon from '../assets/x-base-one-line-user.svg';
-import XBellIcon from '../assets/x-base-one-line-bell.svg';
+import { useState } from 'react';
 
 function Navigation() {
+  const [selectedKey, setSelectedkey] = useState<IconType | undefined>('frame-dash');
   return (
     <>
-      {/* <div class='bg-gray-500 flex flex-col h-screen'>
-        <div class='flex h-32 bg-gray-200'></div>
-        <div class='flex-1 w-2/3 mx-auto p-4 text-lg bg-white h-full shadow-lg bg-gray-300'></div>
-      </div> */}
       <div className='flex flex-row h-full'>
-      <nav className='w-20 flex flex-col bg-black'>
-        <div className=''>
-          <div className='mt-5 mb-5'>
-            <a href='#'>
-              <img src={InstillLogo} className='w-10 mb-3 mx-auto' alt='Instill' />
-            </a>
-          </div>
+        <nav className='w-20 flex flex-col bg-black'>
+          <div className=''>
+            <div className='mt-5 mb-5'>
+              <a href='#'>
+                <span className=''>
+                  <img src={InstillLogo} className=' w-10 mb-3 mx-auto' />
+                </span>
+              </a>
+            </div>
 
-          <div className='mt-4'>
-            <ul>
-              <li className='mb-4'>
-                <a href='#'>
-                  <span className='w-12 rounded-lg bg-slate-700'>
-                    <img src={XFrameDashIcon} className='w-6 mb-3 mx-auto' alt='Instill' />
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </div>
+            <div className='mt-4'>
+              <ul>
+                <NavItem iconType='frame-dash' selectedKey={selectedKey} onSelect={setSelectedkey} />
+              </ul>
+            </div>
 
-          <div className='mt-10'>
-            <ul>
-              <li className='mb-4'>
-                <a href='#'>
-                  <span>
-                    <img src={XLineChartIcon} className='w-6 mb-3 mx-auto' alt='Instill' />
-                  </span>
-                </a>
-              </li>
-              <li className='mb-4'>
-                <a href='#'>
-                  <span>
-                    <img src={XPieChartIcon} className='w-6 mb-3 mx-auto' alt='Instill' />
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </div>
+            <div className='mt-10'>
+              <ul>
+                <NavItem iconType='line-chart' selectedKey={selectedKey} onSelect={setSelectedkey} />
+                <NavItem iconType='pie-chart' selectedKey={selectedKey} onSelect={setSelectedkey} />
+              </ul>
+            </div>
 
-          <div className='mt-10'>
-            <ul>
-              <li className='mb-4'>
-                <a href='#'>
-                  <span>
-                    <img src={XOneWaveIcon} className='w-6 mb-3 mx-auto' alt='Instill' />
-                  </span>
-                </a>
-              </li>
-              <li className='mb-4'>
-                <a href='#'>
-                  <span>
-                    <img src={XPeopleIcon} className='w-6 mb-3 mx-auto' alt='Instill' />
-                  </span>
-                </a>
-              </li>
-              <li className='mb-4'>
-                <a href='#'>
-                  <span>
-                    <img src={XBriefCaseIcon} className='w-6 mb-3 mx-auto' alt='Instill' />
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </div>
+            <div className='mt-10'>
+              <ul>
+                <NavItem iconType='one-wave' selectedKey={selectedKey} onSelect={setSelectedkey} />
+                <NavItem iconType='people' selectedKey={selectedKey} onSelect={setSelectedkey} />
+                <NavItem iconType='brief-case' selectedKey={selectedKey} onSelect={setSelectedkey} />
+              </ul>
+            </div>
 
-          <div className='mt-10'>
-            <ul>
-              <li className='mb-4'>
-                <a href='#'>
-                  <span>
-                    <img src={XBulbIcon} className='w-6 mb-3 mx-auto' alt='Instill' />
-                  </span>
-                </a>
-              </li>
-              <li className='mb-4'>
-                <a href='#'>
-                  <span>
-                    <img src={XInstillIcon} className='w-6 mb-3 mx-auto' alt='Instill' />
-                  </span>
-                </a>
-              </li>
-            </ul>
+            <div className='mt-10'>
+              <ul>
+                <NavItem iconType='bulb' selectedKey={selectedKey} onSelect={setSelectedkey} />
+                <NavItem iconType='instill' selectedKey={selectedKey} onSelect={setSelectedkey} />
+              </ul>
+            </div>
           </div>
-        </div>
-        <div className='h-2/3 flex flex-col justify-center'>
-          <div>
-            <ul>
-              <li className='mb-4'>
-                <a href='#'>
-                  <span>
-                    <img src={XUserIcon} className='w-6 mb-3 mx-auto' alt='Instill' />
-                  </span>
-                </a>
-              </li>
-              <li className='mb-4'>
-                <a href='#'>
-                  <span>
-                    <img src={XBellIcon} className='w-6 mb-3 mx-auto' alt='Instill' />
-                  </span>
-                </a>
-              </li>
-            </ul>
+          <div className='h-2/3 flex flex-col justify-center'>
+            <div>
+              <ul>
+                <NavItem iconType='user' selectedKey={selectedKey} onSelect={setSelectedkey} />
+                <NavItem iconType='bell' selectedKey={selectedKey} onSelect={setSelectedkey} />
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-      <div className='px-16 py-4 text-gray-700 bg-gray-200 h-screen w-screen'></div>
-    </div>
+        </nav>
+        <div className='px-16 py-4 text-gray-700 bg-gray-200 h-screen w-screen'></div>
+      </div>
     </>
   );
 }
---
+++ src/components/ui/NavItem.tsx
@@ -0,0 +1,49 @@
+import { cn } from '@/lib/utils';
+
+import { ReactComponent as XFrameDashIcon } from '../assets/x-base-one-line-frame-dash.svg';
+import { ReactComponent as XLineChartIcon } from '../assets/x-base-one-line-icon.svg';
+import { ReactComponent as XPieChartIcon } from '../assets/x-base-one-line-chart-pie.svg';
+import { ReactComponent as XOneWaveIcon } from '../assets/x-base-one-wave-icon.svg';
+import { ReactComponent as XPeopleIcon } from '../assets/x-base-one-line-people.svg';
+import { ReactComponent as XBriefCaseIcon } from '../assets/x-base-one-line-line-rounded-briefcase.svg';
+import { ReactComponent as XBulbIcon } from '../assets/x-base-one-line-light-bulb.svg';
+import { ReactComponent as XInstillIcon } from '../assets/x-base-one-line-instill-icon.svg';
+import { ReactComponent as XUserIcon } from '../assets/x-base-one-line-user.svg';
+import { ReactComponent as XBellIcon } from '../assets/x-base-one-line-bell.svg';
+
+const iconsObj = {
+  'frame-dash': XFrameDashIcon,
+  'line-chart': XLineChartIcon,
+  'pie-chart': XPieChartIcon,
+  'one-wave': XOneWaveIcon,
+  people: XPeopleIcon,
+  'brief-case': XBriefCaseIcon,
+  bulb: XBulbIcon,
+  instill: XInstillIcon,
+  user: XUserIcon,
+  bell: XBellIcon,
+};
+
+export type IconType = keyof typeof iconsObj;
+
+interface NavItemProps {
+  iconType: IconType;
+  selectedKey?: IconType;
+  url?: string;
+  onSelect: (v?: IconType) => void;
+}
+
+function NavItem({ iconType, selectedKey, url = '', onSelect }: NavItemProps) {
+  const Icon = iconsObj[iconType];
+  const selected = iconType === selectedKey;
+
+  return (
+    <li className={cn('mt-2 mb-2 w-12 h-8 p-1 mx-auto', selected && 'rounded-lg  bg-gray-900')} onClick={() => onSelect?.(iconType)}>
+      <a href={url || '#'}>
+        <span className=''>{Icon ? <Icon className={cn('w-6 mb-3 mx-auto', selected && 'sel-icon')} /> : null}</span>
+      </a>
+    </li>
+  );
+}
+
+export default NavItem;
