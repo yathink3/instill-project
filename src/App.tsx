8fc03a6a0b6a8c6176cd83a8f555aad4129d6c91
yathink3 <yathink3@gmail.com>
2023-08-15 22:30:33 +0800

    (feat)ui : added icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import { useState } from 'react';
-import { Button } from '@/components/ui/button';
+// import BellIcon from './assets/x-base-one-line-bell.svg';
+// import ChartIcon from './assets/x-base-one-line-chart-pie.svg';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+import Navigation from './Pages-components/Navigation';
 
+function App() {
   return (
     <>
-      <div></div>
-      <h1>Vite + React</h1>
+      {/* <div>
+        <img src={BellIcon} className='logo' alt='Bell' />
+        <img src={ChartIcon} className='logo' alt='Chart' />
+      </div>
+      <h1>Instill</h1>
       <div className='card'>
         <Button onClick={() => setCount(count => count + 1)}>count is {count}</Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <p className='read-the-docs'>This is Instill App</p> */}
+      <div>
+        <Navigation />
+      </div>
     </>
   );
 }
